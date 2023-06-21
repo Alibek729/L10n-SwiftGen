f66a3d49ee89ece2b86218dcf4be8258ce696017
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-06-21 11:18:51 +0600

    Added Runs Script in Build Phases for SwiftGen

--- M1L7 CleanSwift/Scenes/LoginScene/LoginViewController.swift
+++ M1L7 CleanSwift/Scenes/LoginScene/LoginViewController.swift
@@ -108,7 +108,7 @@ private extension LoginViewController {
 		button.configuration = .filled()
 		button.configuration?.cornerStyle = .medium
 		button.configuration?.baseBackgroundColor = .red
-		button.configuration?.title = "Login".localized
+		button.configuration?.title = "Authorization.login".localized
 		button.addTarget(self, action: #selector(login), for: .touchUpInside)
 		button.translatesAutoresizingMaskIntoConstraints = false
 
