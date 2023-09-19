788e9c9588d61fa5b83beeecc91c4812b216c0e7
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-09-19 11:24:13 +0600

    Created HTTPHeader with HeaderField

--- M1L7 CleanSwift/NetworkLayer/Models/HTTPHeader.swift
+++ M1L7 CleanSwift/NetworkLayer/Models/HTTPHeader.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+public typealias HTTPHeader = [String: String]
+
+/// Поля HTTP-заголовка. Содержит список часто используемых полей заголовка для устранения хардкода и избежания ошибок написания полей.
+public enum HeaderField {
+	/// Авторизация, содержит токен и используется для OAuth2 авторизации
+	case authorization(String)
+	/// Тип контента, содержит тип к
+	case contentType(ContentType)
+
+	/// Название ключа загаловка.
+	public var key: String {
+		switch self {
+		case .authorization:
+			return "Authorization"
+		case .contentType:
+			return "Content-Type"
+		}
+	}
+
+	/// Значение поля заголовка.
+	public var value: String {
+		switch self {
+		case .authorization(let token):
+			return "Bearer \(token)"
+		case .contentType(let contentType):
+			return contentType.value
+		}
+	}
+}
