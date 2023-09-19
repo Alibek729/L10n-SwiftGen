4bcbabf483d92cbb5a1c0770ffbd42616d073974
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-09-19 09:56:55 +0600

    Added colors with hex

--- M1L7 CleanSwift/Themes/Colors.swift
+++ M1L7 CleanSwift/Themes/Colors.swift
@@ -5,4 +5,51 @@
 //  Created by Alibek Kozhambekov on 21.06.2023.
 //
 
-import Foundation
+import UIKit
+
+enum FlatColor {
+	enum Green {
+		static let Fern = try! UIColor(hex: 0x6ABB72)
+		static let MountainMeadow = try! UIColor(hex: 0x3ABB9D)
+		static let ChateauGreen = try! UIColor(hex: 0x4DA664)
+		static let PersianGreen = try! UIColor(hex: 0x2CA786)
+	}
+
+	enum Blue {
+		static let PictonBlue = try! UIColor(hex: 0x5CADCF)
+		static let Mariner = try! UIColor(hex: 0x3585C5)
+		static let CuriousBlue = try! UIColor(hex: 0x4590B6)
+		static let Denim = try! UIColor(hex: 0x2F6CAD)
+		static let Chambray = try! UIColor(hex: 0x485675)
+		static let BlueWhale = try! UIColor(hex: 0x29334D)
+	}
+
+	enum Violet {
+		static let Wisteria = try! UIColor(hex: 0x9069B5)
+		static let BlueGem = try! UIColor(hex: 0x533D7F)
+	}
+
+	enum Yellow {
+		static let Energy = try! UIColor(hex: 0xF2D46F)
+		static let Turbo = try! UIColor(hex: 0xF7C23E)
+	}
+
+	enum Orange {
+		static let NeonCarrot = try! UIColor(hex: 0xF79E3D)
+		static let Sun = try! UIColor(hex: 0xEE7841)
+	}
+
+	enum Red {
+		static let TerraCotta = try! UIColor(hex: 0xE66B5B)
+		static let Valencia = try! UIColor(hex: 0xCC4846)
+		static let Cinnabar = try! UIColor(hex: 0xDC5047)
+		static let WellRead = try! UIColor(hex: 0xB33234)
+	}
+
+	enum Gray {
+		static let AlmondFrost = try! UIColor(hex: 0xA28F85)
+		static let WhiteSmoke = try! UIColor(hex: 0xEFEFEF)
+		static let Iron = try! UIColor(hex: 0xD1D5D8)
+		static let IronGray = try! UIColor(hex: 0x75706B)
+	}
+}
