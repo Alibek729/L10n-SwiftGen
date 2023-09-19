a39172a284bec8f455c5e1d131938f07a74bf2b1
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-09-19 14:53:06 +0600

    add func in URLRequest extension adds parameters to request, set func in URLComponents extension sets new parameters to URLComponnts

--- M1L7 CleanSwift/NetworkLayer/Extensions/URLRequest+addFunc.swift
+++ M1L7 CleanSwift/NetworkLayer/Extensions/URLRequest+addFunc.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+extension URLRequest {
+	public mutating func add(header: HeaderField) {
+		setValue(header.value, forHTTPHeaderField: header.key)
+	}
+
+	/// Метод добавляющий параметры
+	/// - Parameters:
+	///  - parameters: параметры типа RequestParameters
+	public mutating func add(parameters: Parameters) {
+		switch parameters {
+		case .none:
+			break
+		case .url(let dictionary):
+			guard let url = url, var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { break }
+			components = components.setParameters(dictionary)
+			guard let newURL = components.url else { break }
+			self.url = newURL
+		case .json(let dictionary):
+			httpBody = try? JSONSerialization.data(withJSONObject: dictionary)
+		case .formData(let dictionary):
+			httpBody = URLComponents().setParameters(dictionary).query?.data(using: .utf8)
+		case .data(let data, _):
+			httpBody = data
+		}
+	}
+}
