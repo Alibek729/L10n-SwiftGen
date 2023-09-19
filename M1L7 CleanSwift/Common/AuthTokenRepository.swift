ea9064cfa59f2e2398c12f29b4335d1e92678939
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-09-19 15:26:55 +0600

    Created AuthTokenRepository that saves, updates, deletes, gets token using KeychainService

--- M1L7 CleanSwift/Common/AuthTokenRepository.swift
+++ M1L7 CleanSwift/Common/AuthTokenRepository.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+protocol IAuthTokenRepository {
+	func getSecret() -> AuthToken?
+	func saveSecret(_ token: AuthToken) -> Bool
+	func deleteSecret() -> Bool
+	func updateSecret(_ token: AuthToken) -> Bool
+}
+
+final class AuthTokenRepository: IAuthTokenRepository {
+	//MARK: - Public properties
+
+	let service: String
+	let account: String
+
+	//MARK: - Initialization
+
+	init(service: String, account: String) {
+		self.service = service
+		self.account = account
+	}
+
+	//MARK: - Public methods
+
+	func getSecret() -> AuthToken? {
+		if let secret = KeychainService().getSecret(service: service, account: account) {
+			return AuthToken(rawValue: secret)
+		} else {
+			return nil
+		}
+	}
+
+	func saveSecret(_ token: AuthToken) -> Bool {
+		return KeychainService().saveSecret(service: service, account: account, secret: token.rawValue)
+	}
+
+	func deleteSecret() -> Bool {
+		return KeychainService().deleteSecret(service: service, account: account)
+	}
+
+	func updateSecret(_ token: AuthToken) -> Bool {
+		KeychainService().updateSecret(service: service, account: account, secret: token.rawValue)
+	}
+}
